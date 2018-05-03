c42f818d15c6d92216d9100ab4b03f084667f3b1
Dustin Milotte <dmilotte@gmail.com>
2018-05-03 17:27:20 -0400

    add stereo oscillators and synth controllers

--- Assets/Scripts/CollisionField.cs
+++ Assets/Scripts/CollisionField.cs
@@ -3,93 +3,98 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CollisionField : MonoBehaviour {
-	
-	//public GameObject RightControllerParticleSystem;
 
-	private ParticleSystem particles;
-	private Oscillator myOsc;
-	private SynthControl mySynthControl;
-	private bool isPlayingParticles = false;
-    private float top, bottom, front, back, left, right;
-
+	public Oscillator leftOsc;
+    public Oscillator rightOsc;
+    public SynthControl leftSynthControl;
+    public SynthControl rightSynthControl;
+    private bool playingSoundLeft = false;
+    private bool playingSoundRight = false;
+    private float bottom, back, left;
     public float noteSpacing = .2f;
 
-
-	void Start () {
-		myOsc = GetComponent<Oscillator>();
-		mySynthControl = GetComponent<SynthControl>();
-		//RightControllerParticleSystem = GameObject.Find("RightControllerParticleSystem");
-		//particles = RightControllerParticleSystem.GetComponent<ParticleSystem>();
-	}
-
     void Update() {
-        top = gameObject.transform.position.y + (gameObject.transform.localScale.y / 2);
         bottom = gameObject.transform.position.y - (gameObject.transform.localScale.y / 2);
-        front = gameObject.transform.position.z + (gameObject.transform.localScale.z / 2);
         back = gameObject.transform.position.z - (gameObject.transform.localScale.z / 2);
         left = gameObject.transform.position.x - (gameObject.transform.localScale.x / 2);
-        right = gameObject.transform.position.x + (gameObject.transform.localScale.x / 2);
     }
 
-	void OnTriggerStay(Collider col){
-        DetermineNote(col);
-        SetVolume(col);
-        SetCutoffFreq(col);
+	void OnTriggerStay(Collider col) {
+        print(col.gameObject.name);
+        if(Input.GetAxis("TriggerLeft") > 0 && col.gameObject.name == "LeftController") {
+            playingSoundLeft = true;
+            PlaySoundLeft(col);
+        }
 
-        //Vector3 colPos = col.gameObject.transform.position;
-        //PlayParticles ();
+        if(Input.GetAxis("TriggerRight") > 0 && col.gameObject.name == "RightController") {
+            playingSoundRight = true;
+            PlaySoundRight(col);
+        }
+
+        if(Input.GetAxis("TriggerLeft") == 0 && playingSoundLeft) {
+            leftOsc.StopSound();
+            playingSoundLeft = false;
+        }
+
+        if(Input.GetAxis("TriggerRight") == 0 && playingSoundRight) {
+            rightOsc.StopSound();
+            playingSoundRight = false;
+        }
     }
 
-    void DetermineNote(Collider col) {
+    void PlaySoundLeft(Collider col) {
+        DetermineNote(col, leftOsc);
+        SetVolume(col, leftSynthControl);
+        SetCutoffFreq(col, leftSynthControl);
+    }
+
+    void PlaySoundRight(Collider col) {
+        DetermineNote(col, rightOsc);
+        SetVolume(col, rightSynthControl);
+        SetCutoffFreq(col, rightSynthControl);
+    }
+
+    void DetermineNote(Collider col, Oscillator osc) {
         Vector3 colPos = col.gameObject.transform.position;
-       
         float distFromBottom = colPos.y - bottom;
-        int localOctave = (int)Mathf.Round(distFromBottom / noteSpacing) / (myOsc.frequencies.Length);
-      
-        int note = (int)(Mathf.Round(distFromBottom / noteSpacing) % (myOsc.frequencies.Length));
+        int localOctave = (int)Mathf.Round(distFromBottom / noteSpacing) / (osc.frequencies.Length);
+        int note = (int)(Mathf.Round(distFromBottom / noteSpacing) % (osc.frequencies.Length));
         print("octave: " + localOctave + " note: " + note);
-       
-        if (note >= 0) myOsc.PlaySound(note, localOctave);
+
+        if(note >= 0) {
+            osc.PlaySound(note, localOctave);
+        }
     }
 
-    void SetVolume(Collider col) {
+    void SetVolume(Collider col, SynthControl sc) {
         Vector3 colPos = col.gameObject.transform.position;
         //determine where collider is within the box from
         float distFromBack = colPos.z - back;
         float localPosZ = distFromBack / gameObject.transform.localScale.z;
         float clampedLocalPosZ = Mathf.Clamp(localPosZ, 0, 1f);
-     
 
-        mySynthControl.SetVolume(Mathf.Lerp(-80f, 0f, clampedLocalPosZ));
+        sc.SetVolume(Mathf.Lerp(-80f, 0f, clampedLocalPosZ));
     }
 
-    void SetCutoffFreq(Collider col) {
+    void SetCutoffFreq(Collider col, SynthControl sc) {
         Vector3 colPos = col.gameObject.transform.position;
         //determine where collider is within the box from
         float distFromLeft = colPos.x - left;
         float localPosX = distFromLeft / gameObject.transform.localScale.x;
         float clampedLocalPosX = Mathf.Clamp(localPosX, 0, 1f);
         //print(clampedLocalPosX);
-
-
-        mySynthControl.SetCutoff(Mathf.Lerp(500f, 15000f, clampedLocalPosX));
+        sc.SetCutoff(Mathf.Lerp(500f, 15000f, clampedLocalPosX));
     }
 
-    void OnTriggerExit(){
-		myOsc.StopSound();
-		//StopParticles ();
-		Debug.Log("triggerexit");
-	}
-
-    void PlayParticles() {
-        if(!isPlayingParticles) {
-            particles.Play();
-            isPlayingParticles = true;
+    void OnTriggerExit(Collider col){
+        if (col.gameObject.name == "LeftController") {
+            leftOsc.StopSound();
         }
-    }
+        if(col.gameObject.name == "RightController") {
+            rightOsc.StopSound();
+        }
 
-    void StopParticles() {
-        particles.Stop();
-        isPlayingParticles = false;
-    }
+        //StopParticles ();
+        Debug.Log("triggerexit");
+	}
 }
